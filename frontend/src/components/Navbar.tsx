4c7c2bb04a5ba44406f917d59431a233ef1434d4
agastya gaur <agastyagaur17@gmail.com>
2024-10-26 22:34:47 +0530

    add-authentication

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,21 @@
 import {assets} from '../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk,UserButton,useUser} from '@clerk/clerk-react'
 const Navbar = () => {
+  const {openSignIn} = useClerk()
+  const {isSignedIn,user}=useUser()
   return (
     <div className=' flex items-center justify-between py-3 mx-4'>
        <Link to={'/'}>
        <img className=' w-32  sm:w-44' src={assets.logo} alt="" /></Link>
-        <button className=' bg-zinc-800 items-center flex gap-4 sm:px-8 sm:py-3 px-4 py-2 text-sm rounded-full text-white'>
+       {
+        isSignedIn? <div>
+          <UserButton/>
+        </div>:(<button onClick={()=>openSignIn({})} className=' bg-zinc-800 items-center flex gap-4 sm:px-8 sm:py-3 px-4 py-2 text-sm rounded-full text-white'>
         Get Start    <img className=' w-3 sm:w-4' src={assets.arrow_icon} alt="" />
-        </button>
+        </button>)
+       }
+        
     </div>
   )
 }
